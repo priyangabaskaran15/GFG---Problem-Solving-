65692a1151dcd8963abe1110905e77b417ebcdc4
Priyanga B <priyangabaskaran2127@gmail.com>
2026-08-01 23:58:01 +0530

    Refactor anagram checking logic for clarity

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,11 +1,11 @@
 class Solution {
     public static boolean areAnagrams(String s1, String s2) {
         // code here
-        String s11[] = s1.split("");
-        String s22[] = s2.split("");
-        Arrays.sort(s11);
-        Arrays.sort(s22);
-        if(Arrays.equals(s11,s22)){
+        String a[] = s1.split("");
+        String b[] = s2.split("");
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.equals(a,b)){
             return true;
         }
         else{
