ee8b64b5888d98febab63f1d80cea637085faae4
Priyanga B <priyangabaskaran2127@gmail.com>
2026-08-14 13:40:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Squares of First n Natural Numbers/sum-of-squares-of-first-n-natural-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    // Function to calculate the sum of squares of first 'number' natural numbers
+    public int sumOfSquares(int number) {
+        // code here
+        int res = 0;
+        for(int i=1;i<=number;i++){
+            res += i*i;
+        }
+        return res;
+    }
+}
