b92f119c99d969ac0aae6ae5825cf98764d18388
Priyanga B <priyangabaskaran2127@gmail.com>
2026-09-12 11:47:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Reverse Traversal/array-reverse-traversal.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    void arrayTraversalReverse(int arr[], int n) {
+        // Code here
+        for(int i=n-1;i>=0;i--){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
