8308cb46650a876bcf6cb67b55b23b7434bfd6a2
Priyanga B <priyangabaskaran2127@gmail.com>
2026-09-17 10:40:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Consonants/remove-consonants.java
@@ -0,0 +1,14 @@
+class Solution {
+    String remConsonants(String s) {
+        // code here
+        String res = "";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
+                res += s.charAt(i);
+            }
+        }
+        return res;
+        
+    }
+}
